1ed41e33fbe835ce0f70daba21bcac8b4ee043b4
Raja Imaya Bharathi <113758147+RajaImayaBharathi@users.noreply.github.com>
2025-04-27 12:58:22 +0530

    Update App.jsx
    port address changed to vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ import { UserContextProvider } from "./context/UserContext";
 import FeaturesGrid from "./pages/Featurespage";
 
 // Axios defaults
-axios.defaults.baseURL = "ai-job-application-tracker-project.vercel.app";
+axios.defaults.baseURL = "https://ai-job-application-tracker-project.vercel.app/";
 axios.defaults.withCredentials = true;
 
 const AppContent = () => {
